565cd94b6cecd342e1ce893639d58358dd643717
Nadim193 <19-41770-3@student.aiub.edu>
2026-01-12 13:59:26 +0600

    Update Firebase credentials configuration to use default path if not set

--- config/services.php
+++ config/services.php
@@ -32,7 +32,7 @@ return [
     ],
 
     'firebase' => [
-        'credentials' => env('FIREBASE_CREDENTIALS'), // full path to service-account json
+        'credentials' => env('FIREBASE_CREDENTIALS') ?: storage_path('app/firebase/firebase_credentials.json'),
         'project_id'  => env('FIREBASE_PROJECT_ID', 'sicip-push-notification'),
     ],
 
